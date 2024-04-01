693886265d4c2cf3a8f7a529008858fb77f35262
EricTheWebDev <eameyaw2023@gmail.com>
2024-04-01 14:44:39 -0500

    solved addb function

---
+++ Solutions/ItzJustEric.js
@@ -0,0 +1,108 @@
+// identity(x) ⇒ any
+// Write a function identity that takes an argument and returns that argument
+
+
+//Write a binary function addb that takes two numbers and returns their sum
+addb = (x,y) => {
+    return x + y
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = {
+    identity,
+    addb,
+    subb,
+    mulb,
+    minb,
+    maxb,
+    add,
+    sub,
+    mul,
+    min,
+    max,
+    addRecurse,
+    mulRecurse,
+    minRecurse,
+    maxRecurse,
+    not,
+    acc,
+    accPartial,
+    accRecurse,
+    fill,
+    fillRecurse,
+    set,
+    identityf,
+    addf,
+    liftf,
+    pure,
+    curryb,
+    curry,
+    inc,
+    twiceUnary,
+    doubl,
+    square,
+    twice,
+    reverseb,
+    reverse,
+    composeuTwo,
+    composeu,
+    composeb,
+    composeTwo,
+    compose,
+    limitb,
+    limit,
+    genFrom,
+    genTo,
+    genFromTo,
+    elementGen,
+    element,
+    collect,
+    filter,
+    filterTail,
+    concatTwo,
+    concat,
+    concatTail,
+    gensymf,
+    gensymff,
+    fibonaccif,
+    counter,
+    revocableb,
+    revocable,
+    extract,
+    m,
+    addmTwo,
+    addm,
+    liftmbM,
+    liftmb,
+    liftm,
+    exp,
+    expn,
+    addg,
+    liftg,
+    arrayg,
+    continuizeu,
+    continuize,
+    vector,
+    exploitVector,
+    vectorSafe,
+    pubsub,
+    mapRecurse,
+    filterRecurse,
+};
